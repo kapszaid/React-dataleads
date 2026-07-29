8779f07a01b4e866120a8f4571328a60ccc3c4f1
kapszaid <286227555+kapszaid@users.noreply.github.com>
2026-07-29 18:04:05 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -65,12 +65,15 @@ app = FastAPI(title="DataLeads API Backend")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "https://dataleads.launchex.net",
     "https://dataleads.pages.dev",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
